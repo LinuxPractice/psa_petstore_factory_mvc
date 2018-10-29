4220e13b51c7a12bddd9bcb93c1e93c428922441
LinuxPractice <william.john.platt@gmail.com>
2018-10-28 23:53:17 -0400

    added viewport
    added viewport

--- app/view/themes/default/common/header.php
+++ app/view/themes/default/common/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
-<meta charset="UTF-8">
 <head>
 <meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <title>PSA Pet Store</title>
 <link rel="stylesheet"
 	href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
